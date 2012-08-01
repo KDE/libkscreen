55aa8bb82b91fb4c4394ee5535c84fdab2d613ea
Alex Fiestas <afiestas@kde.org>
2012-08-01 22:22:24 +0200

    The test for XRandR should be QApplication

--- tests/testxrandr.cpp
+++ tests/testxrandr.cpp
@@ -16,6 +16,8 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
  *************************************************************************************/
 
+#define QT_GUI_LIB
+
 #include <QtTest>
 #include <QtCore/QObject>
 
