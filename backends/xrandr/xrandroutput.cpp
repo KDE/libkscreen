d976d656de2a4f84de86e6b2629233748289fd51
Dan Vrátil <dvratil@redhat.com>
2012-12-11 18:47:38 +0100

    Correctly populate output modes

--- backends/xrandr/xrandroutput.cpp
+++ backends/xrandr/xrandroutput.cpp
@@ -189,12 +189,20 @@ void XRandROutput::updateModes(const XRROutputInfo *outputInfo)
     XRRScreenResources *resources = XRandR::screenResources();
     for (int i = 0; i < outputInfo->nmode; ++i)
     {
-        modeInfo = &resources->modes[i];
-        XRandRMode *mode = new XRandRMode(modeInfo, this);
-        m_modes.insert(modeInfo->id, mode);
+        /* Resources->modes contains all possible modes, we are only interested
+         * in those listed in outputInfo->modes. */
+        for (int j = 0; j < resources->nmode; ++j) {
+            modeInfo = &resources->modes[j];
+            if (modeInfo->id != outputInfo->modes[i]) {
+                continue;
+            }
 
-        if (i == outputInfo->npreferred) {
-            m_preferredMode = modeInfo->id;
+            XRandRMode *mode = new XRandRMode(modeInfo, this);
+            m_modes.insert(modeInfo->id, mode);
+
+            if (i == outputInfo->npreferred) {
+                m_preferredMode = modeInfo->id;
+            }
         }
     }
     XRRFreeScreenResources(resources);
