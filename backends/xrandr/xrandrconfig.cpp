KDE/libkscreen

commit 880e97f03d82a339e3f2dfefbb3ad306abef7c38

Don't call XRRGetOutputPrimary in updateCrtc()
Primary display is not change in RRNotify_CrtcChange event,
so don't modify the flag. Minor optimization.

backends/xrandr/xrandroutput.h
@@ -62,12 +62,19 @@ public:
         PropertyEdid            = 1 << 13,
         PropertyPreferredMode   = 1 << 14
     };
+
+    enum {
+        NoChange = 0,
+        SetPrimary = 1,
+        UnsetPrimary = 2,
+    } PrimaryChange;
+
     Q_DECLARE_FLAGS(Properties, Property)
 
     explicit XRandROutput(int id, bool primary, XRandRConfig *config = 0);
     virtual ~XRandROutput();
 
-    void update(bool primary = false);
+    void update(PrimaryChange primary = NoChange);
 
     int id() const;
     bool isEnabled() const;

backends/xrandr/xrandr.cpp
@@ -102,7 +102,7 @@ void XRandR::updateOutput(RROutput output)
     XRandROutput *xOutput = s_internalConfig->outputs().value(output);
     RROutput primary = XRRGetOutputPrimary(XRandR::display(), XRandR::rootWindow());
 
-    xOutput->update((output == primary));
+    xOutput->update((output == primary) ? XRandROutput::SetPrimary : XRandROutput::UnsetPrimary);
     KScreen::ConfigMonitor::instance()->notifyUpdate();
 }
 
@@ -110,10 +110,12 @@ void XRandR::updateCrtc(RRCrtc crtc)
 {
     XRRCrtcInfo* crtcInfo = XRRCrtc(crtc);
     for (int i = 0; i < crtcInfo->noutput; ++i) {
-        updateOutput(crtcInfo->outputs[i]);
+        XRandROutput *xOutput = s_internalConfig->outputs().value(crtcInfo->outputs[i]);
+        xOutput->update();
     }
-
     XRRFreeCrtcInfo(crtcInfo);
+
+    KScreen::ConfigMonitor::instance()->notifyUpdate();
 }
 
 Config* XRandR::config() const

backends/xrandr/xrandroutput.cpp
@@ -105,16 +105,19 @@ KScreen::Edid *XRandROutput::edid() const
     return m_edid;
 }
 
-void XRandROutput::update(bool primary)
+void XRandROutput::update(PrimaryChange primary)
 {
     XRROutputInfo *outputInfo = XRandR::XRROutput(m_id);
 
     m_changedProperties = 0;
     updateOutput(outputInfo);
 
-    if (m_primary != primary) {
-        m_primary = primary;
-        m_changedProperties |= PropertyPrimary;
+    if (primary != NoChange) {
+        bool setPrimary = (primary == SetPrimary);
+        if (m_primary != setPrimary) {
+            m_primary = setPrimary;
+            m_changedProperties |= PropertyPrimary;
+        }
     }
 
     if (m_changedProperties == 0) {

commit 43219892391271de8327bdc91b8d26c8286f4e4b

Fix compilation issues caused by 880e97f

backends/xrandr/xrandrconfig.cpp
@@ -65,7 +65,7 @@ void XRandRConfig::update()
     XRandROutput::Map::Iterator iter;
     for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
         XRandROutput *output = iter.value();
-        output->update(iter.key() == (int) primary);
+        output->update((iter.key() == (int) primary) ? XRandROutput::SetPrimary : XRandROutput::UnsetPrimary);
     }
 }
 
@@ -116,7 +116,7 @@ void XRandRConfig::applyKScreenConfig(KScreen::Config *config)
     QHash<int, int> currentCrtc;
     Q_FOREACH(KScreen::Output *output, outputs) {
         XRandROutput *currentOutput = m_outputs.value(output->id());
-        currentOutput->update(currentOutput->isPrimary());
+        currentOutput->update(currentOutput->isPrimary() ? XRandROutput::SetPrimary : XRandROutput::UnsetPrimary);
 
         if (output->isPrimary()) {
             primaryOutput = currentOutput->id();

backends/xrandr/xrandroutput.h
@@ -63,11 +63,11 @@ public:
         PropertyPreferredMode   = 1 << 14
     };
 
-    enum {
+    enum PrimaryChange {
         NoChange = 0,
         SetPrimary = 1,
         UnsetPrimary = 2,
-    } PrimaryChange;
+    };
 
     Q_DECLARE_FLAGS(Properties, Property)
 