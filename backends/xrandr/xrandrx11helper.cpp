af628438b9425b0a515347beb0e006161745960f
Àlex Fiestas <afiestas@kde.org>
2013-01-14 18:58:00 +0100

    Make XrandrX11Helper to use kDebug(xrandr) instead of an en var

--- backends/xrandr/xrandrx11helper.cpp
+++ backends/xrandr/xrandrx11helper.cpp
@@ -21,23 +21,19 @@
 #include "xlibandxrandr.h"
 
 #include <KSystemEventFilter>
-#include <QDebug>
+#include <kdebug.h>
 
 XRandRX11Helper::XRandRX11Helper():
     QWidget()
 {
-    m_debugMode = (getenv("KSCREEN_XRANDR_DEBUG") != NULL);
-
     XRRQueryVersion (XRandR::display(), &m_versionMajor, &m_versionMinor);
-    if (m_debugMode) {
-        qDebug().nospace() << "Detected XRandR " << m_versionMajor << "." << m_versionMinor;
-    }
+
+    kDebug(dXndr()).nospace() << "Detected XRandR " << m_versionMajor << "." << m_versionMinor;
 
     XRRQueryExtension(XRandR::display(), &m_randrBase, &m_randrError);
-    if (m_debugMode) {
-        qDebug() << "Event Base: " << m_randrBase;
-        qDebug() << "Event Error: "<< m_randrError;
-    }
+
+    kDebug(dXndr()) << "Event Base: " << m_randrBase;
+    kDebug(dXndr()) << "Event Error: "<< m_randrError;
 
 
     m_window = XCreateSimpleWindow(XRandR::display(), XRandR::rootWindow(), 0, 0, 1, 1, 0, 0, 0 );
@@ -91,22 +87,19 @@ bool XRandRX11Helper::x11Event(XEvent *event)
     /* XRandR <= 1.1 */
     if (m_versionMajor >= 1 && m_versionMinor <= 1) {
         if (event->xany.type == m_randrBase + RRScreenChangeNotify) {
-            qDebug() << "RRScreenChangeNotify";
+            KDebug::Block changeNotify("RRScreenChangeNotify", dXndr());
             Q_EMIT outputsChanged();
 
             XRRScreenChangeNotifyEvent* e2 = reinterpret_cast< XRRScreenChangeNotifyEvent* >(event);
 
-            if (m_debugMode) {
-                qDebug() << "\tTimestamp: " << e2->timestamp;
-                qDebug() << "\tWindow: " << e2->window;
-                qDebug() << "\tRoot: "<< e2->root;
-                qDebug() << "\tSize Index: " << e2->size_index;
-                qDebug() << "\tSubpixel Order:" << e2->subpixel_order;
-                qDebug() << "\tRotation: " << rotationToString(e2->rotation);
-                qDebug() << "\tSize: " << e2->width << e2->height;
-                qDebug() << "\tSizeMM: " << e2->mwidth << e2->mheight;
-                qDebug();
-            }
+            kDebug(dXndr()) << "Timestamp: " << e2->timestamp;
+            kDebug(dXndr()) << "Window: " << e2->window;
+            kDebug(dXndr()) << "Root: "<< e2->root;
+            kDebug(dXndr()) << "Size Index: " << e2->size_index;
+            kDebug(dXndr()) << "Subpixel Order:" << e2->subpixel_order;
+            kDebug(dXndr()) << "Rotation: " << rotationToString(e2->rotation);
+            kDebug(dXndr()) << "Size: " << e2->width << e2->height;
+            kDebug(dXndr()) << "SizeMM: " << e2->mwidth << e2->mheight;
         }
 
         return false;
@@ -118,46 +111,37 @@ bool XRandRX11Helper::x11Event(XEvent *event)
         if (e2->subtype == RRNotify_CrtcChange) {
             XRRCrtcChangeNotifyEvent* e2 = reinterpret_cast< XRRCrtcChangeNotifyEvent* >(event);
 
-            if (m_debugMode) {
-                qDebug() << "RRNotify_CrtcChange";
-                qDebug() << "\tCRTC: " << e2->crtc;
-                qDebug() << "\tMode: " << e2->mode;
-                qDebug() << "\tRotation: " << rotationToString(e2->rotation);
-                qDebug() << "\tGeometry: " << e2->x << e2->y << e2->width << e2->height;
-                qDebug();
-            }
+            KDebug::Block crtcChange("RRNotify_CrtcChange", dXndr());
+            kDebug(dXndr()) << "CRTC: " << e2->crtc;
+            kDebug(dXndr()) << "Mode: " << e2->mode;
+            kDebug(dXndr()) << "Rotation: " << rotationToString(e2->rotation);
+            kDebug(dXndr()) << "Geometry: " << e2->x << e2->y << e2->width << e2->height;
 
             Q_EMIT crtcChanged(e2->crtc);
 
         } else if (e2->subtype == RRNotify_OutputChange) {
             XRROutputChangeNotifyEvent* e2 = reinterpret_cast< XRROutputChangeNotifyEvent* >(event);
 
-            if (m_debugMode) {
-                qDebug() << "RRNotify_OutputChange";
-                qDebug() << "\tOutput: " << e2->output;
-                qDebug() << "\tCRTC: " << e2->crtc;
-                qDebug() << "\tMode: " << e2->mode;
-                qDebug() << "\tRotation: " << rotationToString(e2->rotation);
-                qDebug() << "\tConnection: " << connectionToString(e2->connection);
-                qDebug() << "\tSubpixel Order: " << e2->subpixel_order;
-                qDebug();
-            }
+            KDebug::Block outputChange("RRNotify_OutputChange", dXndr());
+            kDebug(dXndr()) << "Output: " << e2->output;
+            kDebug(dXndr()) << "CRTC: " << e2->crtc;
+            kDebug(dXndr()) << "Mode: " << e2->mode;
+            kDebug(dXndr()) << "Rotation: " << rotationToString(e2->rotation);
+            kDebug(dXndr()) << "Connection: " << connectionToString(e2->connection);
+            kDebug(dXndr()) << "Subpixel Order: " << e2->subpixel_order;
 
             Q_EMIT outputChanged(e2->output);
 
         } else if (e2->subtype == RRNotify_OutputProperty) {
             XRROutputPropertyNotifyEvent* e2 = reinterpret_cast< XRROutputPropertyNotifyEvent* >(event);
 
-            if (m_debugMode) {
-                char *atom_name = XGetAtomName(XRandR::display(), e2->property);
-                qDebug() << "RRNotify_Property";
-                qDebug() << "\tTimestamp: " << e2->timestamp;
-                qDebug() << "\tOutput: " << e2->output;
-                qDebug() << "\tProperty: " << XGetAtomName(XRandR::display(), e2->property);
-                qDebug() << "\tState (newValue, Deleted): " << e2->state;
-                qDebug();
-                XFree(atom_name);
-            }
+            char *atom_name = XGetAtomName(XRandR::display(), e2->property);
+            KDebug::Block changeProperty("RRNotify_Property", dXndr());
+            kDebug(dXndr()) << "Timestamp: " << e2->timestamp;
+            kDebug(dXndr()) << "Output: " << e2->output;
+            kDebug(dXndr()) << "Property: " << XGetAtomName(XRandR::display(), e2->property);
+            kDebug(dXndr()) << "State (newValue, Deleted): " << e2->state;
+            XFree(atom_name);
         }
     }
 
