405dc41086f4f11b1c0ba9302f1397825eac7cea
Sebastian KÃgler <sebas@kde.org>
2014-07-23 04:56:21 +0200

    QScreen backend skeleton
    This is an experiment to use QScreen as KScreen backend, this is
    hopefully one day useful when QSceen doesn't suck anymore, in the
    meantime, this is useful to work on Wayland support.
    
    This backend just builds, but does absolutely nothing right now. It will
    probably crash.

---
+++ backends/qscreen/qscreen.cpp
@@ -0,0 +1,99 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *  Copyright (C) 2012, 2013 by Daniel Vrátil <dvratil@redhat.com>                   *
+ *                                                                                   *
+ *  This library is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU Lesser General Public                       *
+ *  License as published by the Free Software Foundation; either                     *
+ *  version 2.1 of the License, or (at your option) any later version.               *
+ *                                                                                   *
+ *  This library is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ *  Lesser General Public License for more details.                                  *
+ *                                                                                   *
+ *  You should have received a copy of the GNU Lesser General Public                 *
+ *  License along with this library; if not, write to the Free Software              *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
+ *************************************************************************************/
+
+#include "qscreen.h"
+#include <configmonitor.h>
+
+#include <QtCore/QFile>
+#include <QtCore/qplugin.h>
+#include <QtCore/QRect>
+#include <QAbstractEventDispatcher>
+
+#include <QX11Info>
+#include <QGuiApplication>
+
+
+
+using namespace KScreen;
+
+Q_LOGGING_CATEGORY(KSCREEN_QSCREEN, "kscreen.qscreen");
+
+QScreen::QScreen(QObject* parent)
+    : QObject(parent)
+    , m_isValid(false)
+{
+    QLoggingCategory::setFilterRules(QLatin1Literal("kscreen.xrandr.debug = true"));
+
+
+}
+
+QScreen::~QScreen()
+{
+
+
+}
+
+QString QScreen::name() const
+{
+    return QString("qscreen");
+}
+
+void QScreen::updateConfig()
+{
+    //s_internalConfig->update();
+    KScreen::ConfigMonitor::instance()->notifyUpdate();
+}
+
+void QScreen::outputRemovedSlot()
+{
+    KScreen::ConfigMonitor::instance()->notifyUpdate();
+}
+
+Config* QScreen::config() const
+{
+    return 0;
+}
+
+void QScreen::setConfig(Config* config) const
+{
+    if (!config) {
+        return;
+    }
+
+}
+
+Edid *QScreen::edid(int outputId) const
+{
+    return 0;
+    //return output->edid();
+}
+
+bool QScreen::isValid() const
+{
+    return m_isValid;
+}
+
+void QScreen::updateConfig(Config *config) const
+{
+    Q_ASSERT(config != 0);
+
+}
+
+
+#include "qscreen.moc"
---
+++ backends/qscreen/qscreen.h
@@ -0,0 +1,60 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *  Copyright (C) 2012, 2013 by Daniel VrÃ¡til <dvratil@redhat.com>                   *
+ *  Copyright 2014 Sebastian Kügler <sebas@kde.org>                                  *
+ *                                                                                   *
+ *  This library is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU Lesser General Public                       *
+ *  License as published by the Free Software Foundation; either                     *
+ *  version 2.1 of the License, or (at your option) any later version.               *
+ *                                                                                   *
+ *  This library is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ *  Lesser General Public License for more details.                                  *
+ *                                                                                   *
+ *  You should have received a copy of the GNU Lesser General Public                 *
+ *  License along with this library; if not, write to the Free Software              *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
+ *************************************************************************************/
+
+#ifndef QSCREEN_BACKEND_H
+#define QSCREEN_BACKEND_H
+
+#include "../abstractbackend.h"
+
+#include <QtCore/QSize>
+#include <QLoggingCategory>
+
+namespace KScreen {
+    class Output;
+}
+
+class QScreen : public QObject, public AbstractBackend
+{
+    Q_OBJECT
+    Q_INTERFACES(AbstractBackend)
+    Q_PLUGIN_METADATA(IID "org.kf5.kscreen.backends.qscreen")
+
+    public:
+        explicit QScreen(QObject* parent = 0);
+        virtual ~QScreen();
+
+        virtual QString name() const;
+        virtual KScreen::Config* config() const;
+        virtual void setConfig(KScreen::Config* config) const;
+        virtual bool isValid() const;
+        virtual KScreen::Edid *edid(int outputId) const;
+        virtual void updateConfig(KScreen::Config *config) const;
+
+    private Q_SLOTS:
+        void updateConfig();
+        void outputRemovedSlot();
+
+
+    private:
+        bool m_isValid;
+};
+
+Q_DECLARE_LOGGING_CATEGORY(KSCREEN_QSCREEN)
+#endif //XRandR_BACKEND_H
