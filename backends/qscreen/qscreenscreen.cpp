8eacd4e3ef0b9895338176a59c3aec9947323d48
Sebastian KÃgler <sebas@kde.org>
2014-08-05 03:58:55 +0200

    Factor out Screen

--- backends/qscreen/qscreenscreen.cpp
+++ backends/qscreen/qscreenscreen.cpp
@@ -16,9 +16,8 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
  *************************************************************************************/
 
-#include "qscreenconfig.h"
-#include "qscreenoutput.h"
 #include "qscreenscreen.h"
+#include "qscreenoutput.h"
 
 #include <configmonitor.h>
 #include <mode.h>
@@ -34,28 +33,70 @@
 
 using namespace KScreen;
 
-QScreenConfig::QScreenConfig(QObject *parent)
-    : Config(parent)
+static int s_kscreenqscreenbackendScreenId = -1;
+
+int getId()
+{
+    s_kscreenqscreenbackendScreenId++;
+    return s_kscreenqscreenbackendScreenId;
+}
+
+QScreenScreen::QScreenScreen(Config *config)
+    : Screen(config)
+    , m_config(config)
 
 {
-    QLoggingCategory::setFilterRules(QLatin1Literal("kscreen.qscreen.debug = true"));
+    QLoggingCategory::setFilterRules(QLatin1Literal("kscreen.xrandr.debug = true"));
     updateConfig();
+    connect(qApp, &QGuiApplication::screenAdded, this, &QScreenScreen::screenAdded);
 }
 
-QScreenConfig::~QScreenConfig()
+QScreenScreen::~QScreenScreen()
 {
 }
 
-void QScreenConfig::updateConfig()
+void QScreenScreen::screenAdded(QScreen* qscreen)
 {
-    m_screen = new QScreenScreen(this);
-    setScreen(m_screen);
+    qCDebug(KSCREEN_QSCREEN) << "Screen added!!! Updating config..";
+    updateConfig(); // FIXME: We want to be a bit smarter here.
 }
 
-QMap< int, QScreenOutput * > QScreenConfig::outputMap() const
+
+void QScreenScreen::updateConfig()
 {
-    return m_screen->outputMap();
+    setId(getId());
+
+    auto primary = QGuiApplication::primaryScreen();
+    QSize _s = primary->availableVirtualGeometry().size();
+    setMinSize(_s);
+    setMaxSize(_s);
+    setCurrentSize(_s);
+    setMaxActiveOutputsCount(QGuiApplication::screens().count());
+
+    OutputList outputList;
+
+    foreach(const QScreen * qscreen, QGuiApplication::screens()) {
+
+        qCDebug(KSCREEN_QSCREEN) << "New Output: " << qscreen->name();
+
+        QScreenOutput *output = new QScreenOutput(qscreen);
+
+        if (output->isPrimary()) {
+            m_config->setPrimaryOutput(output);
+
+        }
+
+        outputList.insert(output->id(), output);
+        m_outputMap.insert(output->id(), output);
+    }
+
+    m_config->setOutputs(outputList);
 }
 
-#include "qscreenconfig.moc"
+QMap< int, QScreenOutput * > QScreenScreen::outputMap() const
+{
+    return m_outputMap;
+}
+
+#include "qscreenscreen.moc"
 
