c23d6982a78cd66ee985dd0f8b13cd2ddb5e4c0a
Sebastian Kügler <sebas@kde.org>
2014-09-04 04:53:22 +0200

    Test Wayland backend's outputs
    Lots of fail, but we need something to fix.

--- autotests/testwaylandbackend.cpp
+++ autotests/testwaylandbackend.cpp
@@ -38,6 +38,7 @@ private Q_SLOTS:
     void initTestCase();
     void verifyConfig();
     void verifyScreen();
+    void verifyOutputs();
     void cleanupTestCase();
 
 private:
@@ -66,7 +67,7 @@ void testWaylandBackend::verifyConfig()
 void testWaylandBackend::verifyScreen()
 {
     Screen *screen = m_config->screen();
-    qDebug() << "SCreen: " << screen;
+    qDebug() << "Screen: " << screen;
     //return;
     QVERIFY(screen->minSize().width() <= screen->maxSize().width());
     QVERIFY(screen->minSize().height() <= screen->maxSize().height());
@@ -77,6 +78,50 @@ void testWaylandBackend::verifyScreen()
     QVERIFY(screen->maxSize().width() >= screen->currentSize().width());
     QVERIFY(screen->maxSize().height() >= screen->currentSize().height());
     QVERIFY(m_config->screen()->maxActiveOutputsCount() > 0);
+}
+
+void testWaylandBackend::verifyOutputs()
+{
+
+    bool primaryFound = false;
+    foreach (const KScreen::Output* op, m_config->outputs()) {
+        qDebug() << "CHecking at all";
+        if (op->isPrimary()) {
+            primaryFound = true;
+        }
+    }
+    qDebug() << "Primary found? " << primaryFound;
+    QVERIFY(primaryFound);
+    QVERIFY(m_config->outputs().count());
+
+    KScreen::Output *primary = m_config->primaryOutput();
+    qDebug() << "ppp" << primary;
+    QVERIFY(primary->isEnabled());
+    QVERIFY(primary->isConnected());
+    //qDebug() << "Primary geometry? " << primary->geometry();
+    qDebug() << " prim modes: " << primary->modes();
+
+
+    QList<int> ids;
+    foreach (auto output, m_config->outputs()) {
+        qDebug() << " _____________________ Output: " << output;
+        qDebug() << "   output name: " << output->name();
+        qDebug() << "   output modes: " << output->modes().count() << output->modes();
+        qDebug() << "   output enabled: " << output->isEnabled();
+        qDebug() << "   output connect: " << output->isConnected();
+        qDebug() << "   output sizeMm : " << output->sizeMm();
+        QVERIFY(!output->name().isEmpty());
+        QVERIFY(output->id() > -1);
+        QVERIFY(output->isConnected());
+        QVERIFY(output->isEnabled());
+        QVERIFY(output->geometry() != QRectF(1,1,1,1));
+        QVERIFY(output->geometry() != QRectF());
+        QVERIFY(output->sizeMm() != QSize());
+        QVERIFY(output->edid() != 0);
+        QCOMPARE(output->rotation(), Output::None);
+        QVERIFY(!ids.contains(output->id()));
+        ids << output->id();
+    }
 }
 
 void testWaylandBackend::cleanupTestCase()
