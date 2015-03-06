KDE/libkscreen

commit 98d5e9ce8eee211ef84a6144bafb090eb43dd0c1

Add optional check to Config::canBeApplied() to ensure at least one active screen
This adds a new argument to KScreen::Config::canBeApplied() which is a flag
that allows additional optional flags. Specifically this adds a check that
requires the config to have at least one enabled screen in order to be valid.

CCBUG: 330821

src/config.cpp
@@ -51,7 +51,7 @@ class Config::Private
     OutputList outputs;
 };
 
-bool Config::canBeApplied(const ConfigPtr &config)
+bool Config::canBeApplied(const ConfigPtr &config, ValidityFlags flags)
 {
     ConfigPtr currentConfig = BackendManager::instance()->config();
     if (!currentConfig) {
@@ -122,6 +122,11 @@ bool Config::canBeApplied(const ConfigPtr &config)
         if (bottomRight.y() > rect.height()) {
             rect.setHeight(bottomRight.y());
         }
+    }
+
+    if (flags & ValidityFlag::RequireAtLeastOneEnabledScreen && enabledOutputsCount == 0) {
+        qCDebug(KSCREEN) << "canBeAppled: There are no enabled screens, at least one required";
+        return false;
     }
 
     const int maxEnabledOutputsCount = config->screen()->maxActiveOutputsCount();

src/config.h
@@ -50,6 +50,12 @@ class KSCREEN_EXPORT Config : public QObject
     Q_PROPERTY(OutputList outputs READ outputs)
 
   public:
+    enum class ValidityFlag {
+        None = 0x0,
+        RequireAtLeastOneEnabledScreen = 0x1
+    };
+    Q_DECLARE_FLAGS(ValidityFlags, ValidityFlag)
+
     /**
      * Validates that a config can be applied in the current system
      *
@@ -58,9 +64,10 @@ class KSCREEN_EXPORT Config : public QObject
      * can be applied.
      *
      * @arg config to be checked
+     * @flags enable additional optional checks
      * @return true if the configuration can be applied, false if not.
      */
-    static bool canBeApplied(const ConfigPtr &config);
+    static bool canBeApplied(const ConfigPtr &config, ValidityFlags flags = ValidityFlag::None);
 
     /**
      * Instance an empty config

CMakeLists.txt
@@ -1,5 +1,5 @@
 project(libkscreen)
-set(PROJECT_VERSION "5.2.90")
+set(PROJECT_VERSION "5.2.91")
 
 cmake_minimum_required(VERSION 2.8.12)
 

commit 0510f7620a88010a1c57796ef26bb6baa63144db

Fix ABI break introduced in 98d5e9c

src/config.cpp
@@ -51,6 +51,11 @@ class Config::Private
     OutputList outputs;
 };
 
+bool Config::canBeApplied(const ConfigPtr &config)
+{
+    return canBeApplied(config, ValidityFlag::None);
+}
+
 bool Config::canBeApplied(const ConfigPtr &config, ValidityFlags flags)
 {
     ConfigPtr currentConfig = BackendManager::instance()->config();
@@ -92,7 +97,6 @@ bool Config::canBeApplied(const ConfigPtr &config, ValidityFlags flags)
             qCDebug(KSCREEN) << "canBeApplied: The output:" << output->id() << "has no mode:" << output->currentModeId();
             return false;
         }
-
 
         const ModePtr currentMode = output->currentMode();
 

src/config.h
@@ -66,8 +66,21 @@ class KSCREEN_EXPORT Config : public QObject
      * @arg config to be checked
      * @flags enable additional optional checks
      * @return true if the configuration can be applied, false if not.
+     * @since 5.3.0
      */
-    static bool canBeApplied(const ConfigPtr &config, ValidityFlags flags = ValidityFlag::None);
+    static bool canBeApplied(const ConfigPtr &config, ValidityFlags flags);
+
+    /**
+     * Validates that a config can be applied in the current system
+     *
+     * Each system has different constrains, this method will test
+     * the given config with those constrains to see if it
+     * can be applied.
+     *
+     * @arg config to be checked
+     * @return true if the configuration can be applied, false if not.
+     */
+    static bool canBeApplied(const ConfigPtr &config);
 
     /**
      * Instance an empty config