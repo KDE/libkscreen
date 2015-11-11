ec0d7b2b51ce0d79ff1325cff2b4797fa52d1fc1
Sebastian Kügler <sebas@kde.org>
2015-11-11 03:59:09 +0100

    bare-bones test for in-process backend

---
+++ autotests/testinprocess.cpp
@@ -0,0 +1,114 @@
+/*************************************************************************************
+ *  Copyright 2015 by Sebastian Kügler <sebas@kde.org>                           *
+ *                                                                                   *
+ *  This library is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU Lesser General Public                       *
+ *  License as published by the Free Software Foundation; either                     *
+ *  version 2.1 of the License, or (at your option) any later version.               *
+ *                                                                                   *
+ *  This library is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ *  Lesser General Public License for more details.                                  *
+ *                                                                                   *
+ *  You should have received a copy of the GNU Lesser General Public                 *
+ *  License along with this library; if not, write to the Free Software              *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
+ *************************************************************************************/
+
+#include <QCoreApplication>
+#include <QtTest>
+#include <QObject>
+#include <QSignalSpy>
+
+#include <KSharedConfig>
+#include <KConfigGroup>
+
+// KWayland
+#include <KWayland/Client/connection_thread.h>
+#include <KWayland/Client/event_queue.h>
+#include <KWayland/Client/registry.h>
+
+#include <KWayland/Server/compositor_interface.h>
+#include <KWayland/Server/display.h>
+#include <KWayland/Server/output_interface.h>
+#include <KWayland/Server/seat_interface.h>
+#include <KWayland/Server/shell_interface.h>
+
+#include "waylandconfigwriter.h"
+#include "waylandconfigreader.h"
+
+#include "../src/backendmanager_p.h"
+#include "../src/getconfigoperation.h"
+#include "../src/config.h"
+#include "../src/configmonitor.h"
+#include "../src/output.h"
+#include "../src/mode.h"
+#include "../src/edid.h"
+
+#include "../tests/waylandtestserver.h"
+
+Q_LOGGING_CATEGORY(KSCREEN_QSCREEN, "kscreen.wayland");
+
+using namespace KScreen;
+
+class TestInProcess : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit TestInProcess(QObject *parent = nullptr);
+
+private Q_SLOTS:
+    void init();
+    void cleanup();
+
+    void loadConfig();
+
+private:
+
+    ConfigPtr m_config;
+    WaylandTestServer *m_server;
+
+};
+
+TestInProcess::TestInProcess(QObject *parent)
+    : QObject(parent)
+    , m_config(nullptr)
+{
+}
+
+void TestInProcess::init()
+{
+    setenv("KSCREEN_BACKEND", "wayland", 1);
+    KScreen::BackendManager::instance()->shutdownBackend();
+
+    // This is how KWayland will pick up the right socket,
+    // and thus connect to our internal test server.
+    setenv("WAYLAND_DISPLAY", s_socketName.toLocal8Bit(), 1);
+
+    m_server = new WaylandTestServer(this);
+    //m_server->start();
+}
+
+void TestInProcess::cleanup()
+{
+    KScreen::BackendManager::instance()->shutdownBackend();
+    delete m_server;
+}
+
+void TestInProcess::loadConfig()
+{
+    m_server->start();
+    GetConfigOperation *op = new GetConfigOperation();
+    op->exec();
+    m_config = op->config();
+
+    QVERIFY(m_config);
+
+}
+
+
+QTEST_GUILESS_MAIN(TestInProcess)
+
+#include "testinprocess.moc"
