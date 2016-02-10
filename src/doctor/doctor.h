fbd553242832e865a8ba7997076d622950540a9c
Sebastian Kügler <sebas@kde.org>
2016-02-11 00:28:33 +0100

    First bits of kscreen-doctor
    kscreen-doctor is a command-line tool to interact with kscreen. Its
    mainly for testing, but can surely prove useful in the future as a
    replacement for tools such as xrandr.
    
    It can show the output configuration as json for now, not much else.

---
+++ src/doctor/doctor.h
@@ -0,0 +1,59 @@
+/*************************************************************************************
+ *  Copyright 2015 Sebastian Kügler <sebas@kde.org>                                  *
+ *                                                                                   *
+ *  This library is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU Lesser General Public                       *
+ *  License as published by the Free Software Foundation; either                     *
+ *  version 2.1 of the License, or (at your option) any later version.               *
+ *                                                                                   *
+ *  This library is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ *  Lesser General Public License for more details.                                  *
+ *                                                                                   *
+ *  You should have received a copy of the GNU Lesser General Public                 *
+ *  License along with this library; if not, write to the Free Software              *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
+ *************************************************************************************/
+
+#ifndef KSCREEN_DOCTOR_H
+#define KSCREEN_DOCTOR_H
+
+#include <QCommandLineParser>
+#include <QObject>
+#include "../config.h"
+
+namespace KScreen
+{
+class ConfigOperation;
+//static const QString s_socketName = QStringLiteral("libkscreen-test-wayland-backend-0");
+
+class Doctor : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Doctor(QObject *parent = 0);
+    virtual ~Doctor();
+
+    void start(QCommandLineParser *m_parser);
+    void configReceived(KScreen::ConfigOperation *op);
+
+    void showOutputs();
+    void showJson();
+    int outputCount() const;
+
+Q_SIGNALS:
+    void outputsChanged();
+    void started();
+    void configChanged();
+
+private:
+    //static QString modeString(KWayland::Server::OutputDeviceInterface* outputdevice, int mid);
+    KScreen::ConfigPtr m_config;
+    QCommandLineParser* m_parser;
+};
+
+} // namespace
+
+#endif // KSCREEN_WAYLAND_SCREEN_H
