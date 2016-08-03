KDE/libkscreen

commit c2e1becccec9a2cccc80701535733c784f318e5d

update CRTCs before enabling an output
Summary:
When connecting the docking station (Lenovo OneLink+), which has a VGA
connected monitor attached, enabling a display usually fails. It
complains about no CRTC being available (and indeed the available CRTCs
don't have our output as possible output). Updating the CRTC before
enabling an output makes this work for me.

Note: it's not a problem when connecting via HDMI, this seems to be
either unique to VGA or to the docking station. (Other dock users report
similar behaviour).

I'm being fairly liberal with update calls here, probably just doing it
in enableOutput will work as well -- but I haven't extensively tested
it. I don't know how expensive these calls are.

On boot, my setup is still not restored, but at least with this patch
manually enabling an output works reliably now.

BUG:366346

Test Plan:
* rebooted with dock disconnected, can enable an output with this patch, would reliably fail without
* rebooted with dock connected, still works
* connected/disconnected/connected dock with output attached, works well

Reviewers: dvratil, graesslin

Reviewed By: graesslin

Subscribers: graesslin, plasma-devel, #plasma

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D2336

backends/xrandr/xrandrcrtc.cpp
@@ -66,6 +66,7 @@ QVector<xcb_randr_output_t> XRandRCrtc::outputs() const
 
 bool XRandRCrtc::connectOutput(xcb_randr_output_t output)
 {
+    update();
     qCDebug(KSCREEN_XRANDR) << "Connected output" << output << "to CRTC" << m_crtc;
     if (!m_possibleOutputs.contains(output)) {
         qCDebug(KSCREEN_XRANDR) << "Output" << output << "is not an allowed output for CRTC" << m_crtc;
@@ -80,6 +81,7 @@ bool XRandRCrtc::connectOutput(xcb_randr_output_t output)
 
 void XRandRCrtc::disconectOutput(xcb_randr_output_t output)
 {
+    update();
     qCDebug(KSCREEN_XRANDR) << "Disconnected output" << output << "from CRTC" << m_crtc;
     const int index = m_outputs.indexOf(output);
     if (index > -1) {

backends/xrandr/xrandrconfig.cpp
@@ -498,6 +498,7 @@ bool XRandRConfig::enableOutput(const OutputPtr &kscreenOutput) const
     XRandRCrtc *freeCrtc = Q_NULLPTR;
     qCDebug(KSCREEN_XRANDR) << m_crtcs;
     Q_FOREACH (XRandRCrtc *crtc, m_crtcs) {
+        crtc->update();
         qCDebug(KSCREEN_XRANDR) << "Testing CRTC" << crtc->crtc();
         qCDebug(KSCREEN_XRANDR) << "\tFree:" << crtc->isFree();
         qCDebug(KSCREEN_XRANDR) << "\tMode:" << crtc->mode();

commit 6768ece3452ebd62b65b1fc5fadc602b0a14274a

xrandr-crtc: clear outputs in update()
In update, we initialize the entire chain of connected outputs. Since
update() is now called more than once during the life-time of the crtc
object, we need to clear it before filling the vector containing the
connected outputs.

This leads to the problem that crtcs are not freed up, which means we
may run out of crtcs quickly if a display is connected and reconnected.

This fixes a regression introduced with c2e1becccec.

backends/xrandr/xrandrcrtc.cpp
@@ -106,6 +106,8 @@ void XRandRCrtc::update()
     for (int i = 0; i < crtcInfo->num_possible_outputs; ++i) {
         m_possibleOutputs.append(possible[i]);
     }
+
+    m_outputs.clear();
     xcb_randr_output_t *outputs = xcb_randr_get_crtc_info_outputs(crtcInfo);
     for (int i = 0; i < crtcInfo->num_outputs; ++i) {
         m_outputs.append(outputs[i]);