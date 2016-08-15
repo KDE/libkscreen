KDE/libkscreen

commit f53805676dc0e429842074f2339085b67c90d550

[xrandr backend] fall back to preferred mode, fix crash
Summary:
When enabling outputs, it's possible that a KScreen::Output's current
mode is empty. In this case, it's logical and necessary to fall back to
the output's preferred mode, instead of passing essentially
QString().toInt() as mode id into xcb_randr_set_crtc_config().

In the current code, kscreenOuput->currentMode()->size() can crash, this
is fixed with this patch as well.

This change makes enabling a VGA monitor to my docking station more reliable.

Test Plan: Connected, disconnted, reconfigured a tv connected via HDMI.

Reviewers: dvratil, graesslin

Reviewed By: graesslin

Subscribers: graesslin, plasma-devel, #plasma

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D2330

backends/xrandr/xrandrconfig.cpp
@@ -515,17 +515,19 @@ bool XRandRConfig::enableOutput(const OutputPtr &kscreenOutput) const
         return false;
     }
 
+    const int modeId = kscreenOutput->currentMode() ? kscreenOutput->currentModeId().toInt() : kscreenOutput->preferredModeId().toInt();
+
     qCDebug(KSCREEN_XRANDR) << "RRSetCrtcConfig (enable output)";
     qCDebug(KSCREEN_XRANDR) << "\tOutput:" << kscreenOutput->id() << "(" << kscreenOutput->name() << ")";
     qCDebug(KSCREEN_XRANDR) << "\tNew CRTC:" << freeCrtc->crtc();
     qCDebug(KSCREEN_XRANDR) << "\tPos:" << kscreenOutput->pos();
-    qCDebug(KSCREEN_XRANDR) << "\tMode:" << kscreenOutput->currentModeId() << "(" << kscreenOutput->currentMode()->size() << ")";
+    qCDebug(KSCREEN_XRANDR) << "\tMode:" << kscreenOutput->currentMode() << "Preferred:" << kscreenOutput->preferredModeId();
     qCDebug(KSCREEN_XRANDR) << "\tRotation:" << kscreenOutput->rotation();
 
     auto cookie = xcb_randr_set_crtc_config(XCB::connection(), freeCrtc->crtc(),
             XCB_CURRENT_TIME, XCB_CURRENT_TIME,
             kscreenOutput->pos().rx(), kscreenOutput->pos().ry(),
-            kscreenOutput->currentModeId().toInt(),
+            modeId,
             kscreenOutput->rotation(),
             1, outputs);
     XCB::ScopedPointer<xcb_randr_set_crtc_config_reply_t> reply(xcb_randr_set_crtc_config_reply(XCB::connection(), cookie, NULL));
@@ -537,7 +539,7 @@ bool XRandRConfig::enableOutput(const OutputPtr &kscreenOutput) const
 
     if (reply->status == XCB_RANDR_SET_CONFIG_SUCCESS) {
         XRandROutput *xOutput = output(kscreenOutput->id());
-        xOutput->update(freeCrtc->crtc(), kscreenOutput->currentModeId().toInt(),
+        xOutput->update(freeCrtc->crtc(), modeId,
                         XCB_RANDR_CONNECTION_CONNECTED, kscreenOutput->isPrimary());
     }
     return (reply->status == XCB_RANDR_SET_CONFIG_SUCCESS);
@@ -552,11 +554,18 @@ bool XRandRConfig::changeOutput(const OutputPtr &kscreenOutput) const
         return enableOutput(kscreenOutput);
     }
 
+    int modeId = 0;
+    if (!xOutput->currentModeId().isEmpty()) {
+        modeId = xOutput->currentModeId().toInt();
+    } else {
+        modeId = kscreenOutput->currentMode() ? kscreenOutput->currentModeId().toInt() : kscreenOutput->preferredModeId().toInt();
+    }
+
     qCDebug(KSCREEN_XRANDR) << "RRSetCrtcConfig (change output)";
     qCDebug(KSCREEN_XRANDR) << "\tOutput:" << kscreenOutput->id() << "(" << kscreenOutput->name() << ")";
     qCDebug(KSCREEN_XRANDR) << "\tCRTC:" << xOutput->crtc()->crtc();
     qCDebug(KSCREEN_XRANDR) << "\tPos:" << kscreenOutput->pos();
-    qCDebug(KSCREEN_XRANDR) << "\tMode:" << kscreenOutput->currentModeId() << "(" << kscreenOutput->currentMode()->size() << ")";
+    qCDebug(KSCREEN_XRANDR) << "\tMode:" << modeId  << kscreenOutput->currentMode();
     qCDebug(KSCREEN_XRANDR) << "\tRotation:" << kscreenOutput->rotation();
 
     xcb_randr_output_t outputs[1] { static_cast<xcb_randr_output_t>(kscreenOutput->id()) };
@@ -564,7 +573,7 @@ bool XRandRConfig::changeOutput(const OutputPtr &kscreenOutput) const
     auto cookie = xcb_randr_set_crtc_config(XCB::connection(), xOutput->crtc()->crtc(),
             XCB_CURRENT_TIME, XCB_CURRENT_TIME,
             kscreenOutput->pos().rx(), kscreenOutput->pos().ry(),
-            kscreenOutput->currentModeId().toInt(),
+            modeId,
             kscreenOutput->rotation(),
             1, outputs);
     XCB::ScopedPointer<xcb_randr_set_crtc_config_reply_t> reply(xcb_randr_set_crtc_config_reply(XCB::connection(), cookie, NULL));
@@ -575,7 +584,7 @@ bool XRandRConfig::changeOutput(const OutputPtr &kscreenOutput) const
     qCDebug(KSCREEN_XRANDR) << "\tResult: " << reply->status;
 
     if (reply->status == XCB_RANDR_SET_CONFIG_SUCCESS) {
-        xOutput->update(xOutput->crtc()->crtc(), kscreenOutput->currentModeId().toInt(),
+        xOutput->update(xOutput->crtc()->crtc(), modeId,
                         XCB_RANDR_CONNECTION_CONNECTED, kscreenOutput->isPrimary());
     }
     return (reply->status == XCB_RANDR_SET_CONFIG_SUCCESS);

commit a7a6092d8132f34a9a6b5e99d0d9737b41ac2803

fix mode setting when output is already enabled
This fixes setting mode when the output is enabled. In this case, we
have to pick the mode of the KScreen::Config passed in. This falls
back to preferred mode, which will practicably always be valid.

If we consider the xOutput first, we pick the currently set mode and use
that -- hence, nothing is going to be changed. If the display went
through the enabled code path first, the xOutput won't have a mode
already, which is corrected at that point.

This fixes a regression introduced with f53805676d.

Thanks Raymond Wooninck for reporting and testing.

backends/xrandr/xrandrconfig.cpp
@@ -554,12 +554,7 @@ bool XRandRConfig::changeOutput(const OutputPtr &kscreenOutput) const
         return enableOutput(kscreenOutput);
     }
 
-    int modeId = 0;
-    if (!xOutput->currentModeId().isEmpty()) {
-        modeId = xOutput->currentModeId().toInt();
-    } else {
-        modeId = kscreenOutput->currentMode() ? kscreenOutput->currentModeId().toInt() : kscreenOutput->preferredModeId().toInt();
-    }
+    int modeId = kscreenOutput->currentMode() ? kscreenOutput->currentModeId().toInt() : kscreenOutput->preferredModeId().toInt();
 
     qCDebug(KSCREEN_XRANDR) << "RRSetCrtcConfig (change output)";
     qCDebug(KSCREEN_XRANDR) << "\tOutput:" << kscreenOutput->id() << "(" << kscreenOutput->name() << ")";