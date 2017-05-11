2bafd15756a12417d84f6041cbd759813738cc58
David Edmundson <kde@davidedmundson.co.uk>
2017-05-11 16:37:11 +0200

    Update unit test to match scaling
    Summary:
    Scaling wasn't implemented before, so the test passed despite being
    technically wrong.
    
    Test Plan: Passes
    
    Reviewers: #plasma
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D5813

--- autotests/testscreenconfig.cpp
+++ autotests/testscreenconfig.cpp
@@ -156,7 +156,7 @@ void testScreenConfig::multiOutput()
     QCOMPARE(output->type(), Output::HDMI);
     QCOMPARE(output->modes().count(), 4);
     QCOMPARE(output->pos(), QPoint(1280, 0));
-    QCOMPARE(output->geometry(), QRect(1280, 0, 1920, 1080));
+    QCOMPARE(output->geometry(), QRect(1280, 0, 1920 / 1.4, 1080 / 1.4));
     QCOMPARE(output->currentModeId(), QLatin1String("4"));
     QCOMPARE(output->preferredModeId(), QLatin1String("4"));
     QCOMPARE(output->rotation(), Output::None);
