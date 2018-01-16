6e37f3034f6ce68b3b7899a59dbdf13e6028ad86
Daniel Vrátil <dvratil@kde.org>
2018-01-16 17:18:41 +0100

    Don't create multiple in-process backend instances
    Summary:
    The 'name' argument is in most cases empty by which the caller indicates that
    they want the default backend. As such repeated calls to loadBackendInProcess(QString())
    cause to instantiate a new backend every time, because the name check in the
    condition is never true.
    
    Reviewers: sebas, davidedmundson
    
    Reviewed By: davidedmundson
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D9905

--- src/backendmanager.cpp
+++ src/backendmanager.cpp
@@ -226,7 +226,7 @@ KScreen::AbstractBackend *BackendManager::loadBackendInProcess(const QString &na
         qCWarning(KSCREEN) << "You are trying to load a backend in process, while the BackendManager is set to use OutOfProcess communication. Use loadBackendPlugin() instead.";
         return nullptr;
     }
-    if (m_inProcessBackend.first != nullptr && m_inProcessBackend.first->name() == name) {
+    if (m_inProcessBackend.first != nullptr && (name.isEmpty() || m_inProcessBackend.first->name() == name)) {
         return m_inProcessBackend.first;
     } else if (m_inProcessBackend.first != nullptr && m_inProcessBackend.first->name() != name) {
         shutdownBackend();
