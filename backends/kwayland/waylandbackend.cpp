f58b832e4d386a56d6515cc03681c3e7182de20e
Xaver Hugl <xaver.hugl@gmail.com>
2022-10-03 02:07:46 +0200

    backends/kwayland: use primary output for Xwayland scale
    It's more likely to be fitting for the user

--- backends/kwayland/waylandbackend.cpp
+++ backends/kwayland/waylandbackend.cpp
@@ -44,8 +44,11 @@ WaylandBackend::WaylandBackend()
         if (xwaylandClientsScale) {
             qreal scaleFactor = 1;
             const auto outputs = newConfig->outputs();
-            for (auto output : outputs) {
-                scaleFactor = std::max(scaleFactor, output->scale());
+            const auto primaryOutput = std::find_if(outputs.begin(), outputs.end(), [](const OutputPtr &output) {
+                return output->isPrimary();
+            });
+            if (primaryOutput != outputs.end()) {
+                scaleFactor = (*primaryOutput)->scale();
             }
 
             xwaylandGroup.writeEntry("Scale", scaleFactor, KConfig::Notify);
