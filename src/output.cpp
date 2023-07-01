b51a11b9030f70af2f114470bebf0ae70c082d9e
ivan tkachenko <me@ratijas.tk>
2023-07-01 13:33:48 +0300

    Guard the deprecated Output::setPrimary
    Unsetting primary state directly on an output is not supported, but at
    least we should properly warn about it instead of silently ignoring
    the meaningless argument.
    
    BUG: 471195
    FIXED-IN: 5.27.7
    (cherry picked from commit a302c17e95df535bde9232516092d77b34143503)

--- src/output.cpp
+++ src/output.cpp
@@ -7,6 +7,7 @@
 
 #include "output.h"
 #include "edid.h"
+#include "kscreen_debug.h"
 #include "mode.h"
 
 #include <QCryptographicHash>
@@ -496,7 +497,11 @@ bool Output::isPrimary() const
 
 void Output::setPrimary(bool primary)
 {
-    setPriority(1);
+    if (primary) {
+        setPriority(1);
+    } else {
+        qCWarning(KSCREEN) << "Calling Output::setPrimary(false) is not supported. Port your code to Config::setPrimaryOutput";
+    }
 }
 
 uint32_t Output::priority() const
